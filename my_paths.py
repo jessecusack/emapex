41dd23d452e43f0f55e425f75ff129822bbefe9f
jessecusack <jesse.cusack@noc.soton.ac.uk>
2016-09-28 12:09:00 +0100

    corrected emapex data path

--- my_paths.py
+++ my_paths.py
@@ -5,5 +5,5 @@ Created on Sat Sep 17 15:03:01 2016
 @author: Jesse
 """
 
-emapex_data = 'E:\Work\Data\EM-APEX'
-processed = 'E:\Work\Data\Processed'
+emapex_data = '/noc/users/jc3e13/storage/DIMES/EM-APEX'
+processed = '/noc/users/jc3e13/storage/processed'
